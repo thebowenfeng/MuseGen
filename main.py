02331caca13c4abd5d73137c93c88f0f4282c9e4
thebowenfeng <857514.leofeng@gmail.com>
2021-09-23 23:21:31 +1000

    added more midi

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from read_midi import Preprocess
 from network import Bidirectional_Model, RNN_Model
 
-processed = Preprocess("alla-turca.mid")
+processed = Preprocess("moonlight1.mid")
 mymodel = Bidirectional_Model(processed)
-mymodel.train(epoch=70)
-mymodel.generate(num_notes=200, prompt=[1, 2, 3], output_file="new.mid", tempo=1000000)
+mymodel.train(epoch=100)
+mymodel.generate(num_notes=200, prompt=[1, 2, 3], output_file="new.mid", tempo=2000000)
 # Tempo is a crochet (quarter note)'s length in microseconds.
